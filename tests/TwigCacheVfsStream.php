af8d772bb859f3ab9e457f3a35bc5be9c5cb88ac
odan <odan@users.noreply.github.com>
2018-02-11 13:50:28 +0100

    Fixed some phpstan issues

--- tests/TwigCacheVfsStream.php
+++ tests/TwigCacheVfsStream.php
@@ -5,32 +5,6 @@ namespace Odan\Test;
 use RuntimeException;
 use Twig_Cache_Filesystem;
 
-// Shim to not throw "Function opcache_invalidate not found" error when opcache is not enabled
-if (!function_exists('\opcache_invalidate')) {
-    /**
-     * @param string $script
-     * @param bool $force
-     *
-     * @return void
-     */
-    function opcache_invalidate($script, $force = false)
-    {
-    }
-}
-
-// Shim to not throw "Function apc_compile_file not found" error when opcache is not enabled
-if (!function_exists('\apc_compile_file')) {
-    /**
-     * @param string $filename
-     * @param bool $atomic
-     *
-     * @return void
-     */
-    function apc_compile_file(string $filename, bool $atomic = true)
-    {
-    }
-}
-
 /**
  * Class TwigCacheVfs
  */
@@ -64,16 +38,6 @@ class TwigCacheVfsStream extends Twig_Cache_Filesystem
 
         if (false !== @file_put_contents($tmpFile, $content) && @rename($tmpFile, $key)) {
             @chmod($key, 0666 & ~umask());
-
-            if (self::FORCE_BYTECODE_INVALIDATION == ($this->options & self::FORCE_BYTECODE_INVALIDATION)) {
-                // Compile cached file into bytecode cache
-                if (function_exists('opcache_invalidate')) {
-                    opcache_invalidate($key, true);
-                } elseif (function_exists('apc_compile_file')) {
-                    apc_compile_file($key);
-                }
-            }
-
             return;
         }
 
