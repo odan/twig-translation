f573b4fc74f1780d4ce4cc29d53bef03608279e8
odan <d.opitz@outlook.com>
2019-08-25 23:51:34 +0200

    Update build

--- .cs.php
+++ .cs.php
@@ -10,19 +10,14 @@ return PhpCsFixer\Config::create()
         '@Symfony' => true,
         'psr4' => true,
         // custom rules
-        'align_multiline_comment' => true, // psr-5
+        'align_multiline_comment' => ['comment_type' => 'phpdocs_only'], // psr-5
+        'phpdoc_to_comment' => false,
         'array_indentation' => true,
         'array_syntax' => ['syntax' => 'short'],
         'cast_spaces' => ['space' => 'none'],
         'concat_space' => ['spacing' => 'one'],
         'compact_nullable_typehint' => true,
         'declare_equal_normalize' => ['space' => 'single'],
-        'general_phpdoc_annotation_remove' => [
-            'annotations' => [
-                'author',
-                'package',
-            ],
-        ],
         'increment_style' => ['style' => 'post'],
         'list_syntax' => ['syntax' => 'long'],
         'no_short_echo_tag' => true,
@@ -30,9 +25,14 @@ return PhpCsFixer\Config::create()
         'phpdoc_align' => false,
         'phpdoc_no_empty_return' => false,
         'phpdoc_order' => true, // psr-5
+        'phpdoc_no_useless_inheritdoc' => false,
         'protected_to_private' => false,
         'yoda_style' => false,
         'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+            'imports_order' => ['class', 'const', 'function']
+        ],
     ])
     ->setFinder(PhpCsFixer\Finder::create()
         ->in(__DIR__ . '/src')
